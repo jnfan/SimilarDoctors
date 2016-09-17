f2c5f4f574dde444492854eedf2806e15f4bf772
jnfan <jingfan@Jings-MacBook-Air.local>
2016-09-16 22:56:04 -0700

    second commit

--- src/DoctorFinder.java
+++ src/DoctorFinder.java
@@ -1,3 +1,8 @@
+/* This project is to find similar doctors.
+ * Given a doctor, provides a list of similar doctors, in a prioritized order. 
+ * 
+ */
+
 import java.util.*;
 import java.util.Map.Entry;
 
@@ -13,10 +18,16 @@ public class DoctorFinder {
 	//method to find similar doctors
 	public static List<Doctor> findSimilarDoctors(Doctor doctorX, List<Doctor> docList) {
 		List<Doctor> similarDoctors = new ArrayList<>();
-		Queue<Double> overThreshold = new PriorityQueue<>();
 		Map<Doctor, Double> overThresholdEntries = new HashMap<>();
 		double threshold = 0.4999; //only doctors with similar scores over this threshold will be printed out
-		
+		PriorityQueue<Double> overThreshold = new PriorityQueue<Double>(1, new Comparator<Double>() {		
+			public int compare(Double x, Double y) {
+				if (x < y) return 1;
+				if(x > y) return -1;
+				else return 0;
+			}
+		}); // built a reversed-order priority queue which is a max heap
+	
 		/*
 		 * for each doctor in the doctors database, calculate his similar score between him and the argument doctor
 		 * using the two doctor's zip codes, specialties and review scores
@@ -32,8 +43,9 @@ public class DoctorFinder {
 				overThresholdEntries.put(d, similarScore);
 			}
 			
-			//use priority queue to sort similar scores and save the corresponding doctor to the result list
-			for(Double score : overThreshold) {
+			//use the max heap to sort similar scores and save the corresponding doctor to the result list
+			for(int i = 0; i < overThreshold.size(); i++) {
+				Double score = overThreshold.poll();
 				for (Entry<Doctor, Double> entry : overThresholdEntries.entrySet()) {
 			        if (Objects.equals(score, entry.getValue())) {
 			        	similarDoctors.add(entry.getKey());
@@ -47,7 +59,7 @@ public class DoctorFinder {
 	//assume this method can calculate distance between two zip codes
 	//here just uses an example algorithm to calculate it, it should use a more complicated algorithm
 	private static double getDistance(int zip1, int zip2) {
-		double distance = 10 * (zip1 - zip2);
+		double distance = 10 * Math.abs(zip1 - zip2);
 		return distance;
 	}
 	
@@ -70,13 +82,16 @@ public class DoctorFinder {
 		Doctor doctorX = new Doctor("Alex", "Cardiology", 94040, 5); 
 		
 		//create a sample doctor list, to simplify only use few case
-		Doctor doc = new Doctor("Brad", "Neurology", 95051, 1);		
+		Doctor doc1 = new Doctor("Brad", "Neurology", 95051, 1);
+		Doctor doc2 = new Doctor("Chris", "Dentistry", 94043, 3);
 		DoctorFinder example = new DoctorFinder();
 		example.doctorsList.add(doctorX);
-		example.doctorsList.add(doc);
+		example.doctorsList.add(doc1);
+		example.doctorsList.add(doc2);
 		
 		//print out similar doctors
 		List<Doctor> result = new ArrayList<>(findSimilarDoctors(doctorX, example.doctorsList));
 		System.out.println(result.get(0).getName());
+		System.out.println(result.get(1).getName());
 	}
 }
